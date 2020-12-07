e2931288ea16fce14b50df7aa628b25413f35260
meinu-chan <62944053+meinu-chan@users.noreply.github.com>
2020-12-07 13:24:04 +0200

    Camera rotation

--- Assets/Scripts/Environment/CameraMan.cs
+++ Assets/Scripts/Environment/CameraMan.cs
@@ -4,10 +4,34 @@ using UnityEngine;
 
 public class CameraMan : MonoBehaviour
 {
-    [SerializeField] private Transform target;
+    [SerializeField] Transform target;
+    [SerializeField] float rotationSpeed = 50;
+    [SerializeField] float zoomSpeed = 1000;
+    [SerializeField] Camera _camera;
+    private Vector3 rotation;
 
-    void LateUpdate()
+
+    private void Start() {
+    }
+    private void LateUpdate()
     {
         transform.position = target.position;
+        RotateCamera();
+        // CameraZoom();
     }
+
+    private void RotateCamera(){
+        rotation = transform.eulerAngles;
+        rotation.y += Input.GetAxis("Horizontal") * -rotationSpeed * Time.deltaTime;
+        transform.eulerAngles = rotation;
+    }
+
+    // private void CameraZoom(){
+
+    //     Vector3 cameraZoom = _camera.transform.position;
+    //     cameraZoom += new Vector3(-1, 1 ,-1) * Input.GetAxis("Mouse ScrollWheel") * -zoomSpeed * Time.deltaTime;
+
+    //     _camera.transform.position = cameraZoom;
+
+    // }
 }
